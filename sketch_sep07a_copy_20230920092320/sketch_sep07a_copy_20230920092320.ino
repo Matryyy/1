ae0488fb6621648e6b756cd21fc6b2e437ae5cfb
Matryyy <145659601+Matryyy@users.noreply.github.com>
2023-09-21 12:18:50 +0200

    Update sketch_sep07a_copy_20230920092320.ino

--- sketch_sep07a_copy_20230920092320/sketch_sep07a_copy_20230920092320.ino
+++ sketch_sep07a_copy_20230920092320/sketch_sep07a_copy_20230920092320.ino
@@ -48,6 +48,19 @@ void loop() {
     stav = 1 - stav;  
     delay(100);       
   }
+  {
+    delay(2000);
+    //Read data and store it to variables hum and temp
+    hum = dht.readHumidity();
+    temp= dht.readTemperature();
+    //Print temp and humidity values to serial monitor
+    Serial.print("Humidity: ");
+    Serial.print(hum);
+    Serial.print(" %, Temp: ");
+    Serial.print(temp);
+    Serial.println(" Celsius");
+    delay(10000); //Delay 2 sec.
+}
 
   if (stav == 0) {
     int hodnotaFotorezistoru = analogRead(fotorezistorPin);  
