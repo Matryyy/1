632e5aa6127b4efad5fb7b8f9e25a9fa56023207
Matryyy <mates.ct@gmail.com>
2023-09-21 12:07:47 +0200

    1

---
+++ sketch_sep07a_copy_20230920092320/sketch_sep07a_copy_20230920092320.ino
@@ -0,0 +1,91 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+//#include <DHT.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);  
+int fotorezistorPin = A0;             
+int diodaPin = 9;                     
+int diodaPin2 = 10;
+int dioda1 = 4; 
+int dioda2 = 5;
+int dioda3 = 6;
+int tlacitko = 11;
+int stav = 0;  
+
+#define DHTPIN 7          // Pin, ke kterému je připojen senzor teploty
+#define DHTTYPE DHT11     // Typ senzoru (DHT11 nebo DHT22)
+//DHT dht(DHTPIN, DHTTYPE);   // Inicializace DHT senzoru
+
+void Print_Display(int radek, int sloupec, bool clear, String text, bool back){
+  lcd.setCursor(sloupec, radek);
+  if (clear) {
+    lcd.clear();
+  }
+  lcd.print(text);
+}
+
+void setup() {
+  lcd.init();                      
+  lcd.backlight();                 
+  lcd.setCursor(0, 0);            
+  lcd.print("svetlo:"); 
+
+  pinMode(fotorezistorPin, INPUT); 
+  pinMode(diodaPin, OUTPUT);       
+  pinMode(diodaPin2, OUTPUT);
+  pinMode(dioda1, OUTPUT);
+  pinMode(dioda2, OUTPUT);
+  pinMode(dioda3, OUTPUT);
+  pinMode(tlacitko, INPUT);
+
+//  dht.begin();
+}
+
+void loop() {
+  int tlacitkoStisknuto = digitalRead(tlacitko);  
+
+  if (tlacitkoStisknuto == HIGH) {
+    stav = 1 - stav;  
+    delay(100);       
+  }
+
+  if (stav == 0) {
+    int hodnotaFotorezistoru = analogRead(fotorezistorPin);  
+
+    lcd.setCursor(8, 0);                                  
+    lcd.print(hodnotaFotorezistoru);                       
+
+    if (hodnotaFotorezistoru < 20) {
+      digitalWrite(diodaPin, HIGH);  
+      digitalWrite(diodaPin2, LOW);
+      digitalWrite(dioda1, HIGH);
+      digitalWrite(dioda2, LOW);
+      digitalWrite(dioda3, LOW);
+    } else {
+      digitalWrite(diodaPin, LOW);   
+      digitalWrite(diodaPin2, HIGH);
+      digitalWrite(dioda1, LOW);
+      digitalWrite(dioda2, HIGH);
+      digitalWrite(dioda3, HIGH);
+    }
+
+    // Čtení teploty ze senzoru DHT
+//    float teplota = dht.readTemperature();
+
+    // Zobrazte teplotu na LCD displeji
+    lcd.setCursor(0, 1);
+//    lcd.print("Teplota: " + String(teplota) + " C");
+  } else {
+    digitalWrite(dioda1, HIGH);
+    digitalWrite(dioda2, HIGH);
+    digitalWrite(dioda3, HIGH);
+
+    digitalWrite(diodaPin, HIGH);
+    digitalWrite(diodaPin2, HIGH);
+    delay(100);
+    digitalWrite(diodaPin, LOW);
+    digitalWrite(diodaPin2, LOW);
+  }
+
+  delay(100);  
+}
